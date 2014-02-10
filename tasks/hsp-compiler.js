f582f65ffe90bbd69e072965c65b7d8df83fc0e1
pk1a <pkozlowski@amadeus.com>
2014-02-10 17:37:31 +0100

    fix: properly register a Grunt task

--- tasks/hsp-compiler.js
+++ tasks/hsp-compiler.js
@@ -1,34 +1,36 @@
 module.exports = function(grunt) {
   'use strict';
 
-  var compiler = require('hashspace').compiler;
+  grunt.registerMultiTask('hsp-compiler', 'Compile hashspace files.', function() {
 
-  function compileErrMsg(fileName, compileErr) {
-    return 'HSP compilation error in ' + fileName + ' ('+compileErr.line+','+compileErr.column+'): ' + compileErr.description;
-  }
+    var compiler = require('hashspace').compiler;
 
-  this.files.forEach(function (f) {
+    function compileErrMsg(fileName, compileErr) {
+      return 'HSP compilation error in ' + fileName + ' ('+compileErr.line+','+compileErr.column+'): ' + compileErr.description;
+    }
 
-    // filter out invalid file paths
-    var src = f.src.filter(function(filepath){
-      if (!grunt.file.exists(filepath)) {
-        return false;
-      } else {
-        return true;
-      }
-    });
+    this.files.forEach(function (f) {
+      // filter out invalid file paths
+      var src = f.src.filter(function(filepath){
+        if (!grunt.file.exists(filepath)) {
+          return false;
+        } else {
+          return true;
+        }
+      });
 
-    // compile valid paths
-    src.forEach(function (tplFileName) {
+      // compile valid paths
+      src.forEach(function (tplFileName) {
+        var compileResult = compiler.compile(grunt.file.read(tplFileName));
 
-      var compileResult = compiler.compile(grunt.file.read(tplFileName));
+        //TODO: this check won't be needed as soon as https://github.com/ariatemplates/hashspace/issues/61 is fixed
+        if (compileResult.errors.length === 0) {
+          grunt.file.write(f.dest, compileResult.code);
+        } else {
+          grunt.fail.fatal(compileErrMsg(tplFileName, compileResult.errors[0]));
+        }
+      });
 
-      //TODO: this check won't be needed as soon as https://github.com/ariatemplates/hashspace/issues/61 is fixed
-      if (compileResult.errors.length === 0) {
-        grunt.file.write(f.dest, compileResult.code);
-      } else {
-        grunt.fail.fatal(compileErrMsg(tplFileName, compileResult.errors[0]));
-      }
     });
 
   });
