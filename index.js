8f13f4e2156e6bd3dc8e9e5c52b6cafb013b2868
pk1a <pkozlowski@amadeus.com>
2014-02-10 17:12:28 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var grunt = require('grunt');
+var compiler = require('hashspace').compiler;
+
+grunt.registerMultiTask('compile', function () {
+
+  function compileErrMsg(fileName, compileErr) {
+    return 'HSP compilation error in ' + fileName + ' ('+compileErr.line+','+compileErr.column+'): ' + compileErr.description;
+  }
+
+  this.files.forEach(function (f) {
+
+    // filter out invalid file paths
+    var src = f.src.filter(function(filepath){
+      if (!grunt.file.exists(filepath)) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    // compile valid paths
+    src.forEach(function (tplFileName) {
+
+      var compileResult = compiler.compile(grunt.file.read(tplFileName));
+
+      //TODO: this check won't be needed as soon as https://github.com/ariatemplates/hashspace/issues/61 is fixed
+      if (compileResult.errors.length === 0) {
+        grunt.file.write(f.dest, compileResult.code);
+      } else {
+        grunt.fail.fatal(compileErrMsg(tplFileName, compileResult.errors[0]));
+      }
+    });
+
+  });
+});
